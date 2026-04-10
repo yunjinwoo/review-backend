51dcd78fe9fd90874c6604e4f2ebc0aa36ba81ca
DESKTOP-1V3I37K\BIZWIZSYSTEM <yjw3647@gmail.com>
2026-04-10 15:35:34 +0900

    version 추가

--- src/routes/review.ts
+++ src/routes/review.ts
@@ -6,5 +6,8 @@ const router = express.Router();
 router.post('/review', reviewCode);      // 리뷰 생성 및 저장
 router.get('/reviews', getReviews);      // 리뷰 목록 조회
 router.get('/reviews/:id', getReviewById); // 특정 리뷰 상세 조회
+router.get('/version', (req, res) => {
+    res.send('1.0.0');
+});
 
 export default router;
